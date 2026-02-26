8754d5a763edae33dec5ac5aac22f6aa099d0075
Jason Dhakhwa <ext.jason.dhakhwa@lololegacy.com>
2026-02-26 03:37:25 +0000

    library[harpoon]: installed

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,27 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
+
+    -- REQUIRED: Basic setup
+    harpoon:setup()
+
+    -- Add current file to Harpoon list
+    vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon Add File" })
+
+    -- Open the menu
+    vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon Menu" })
+
+    -- Quick jump to files 1-4
+    vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon File 1" })
+    vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon File 2" })
+    vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon File 3" })
+    vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon File 4" })
+
+    -- Cycle through marked files
+    vim.keymap.set("n", "<M-n>", function() harpoon:list():next() end, { desc = "Harpoon Next" })
+    vim.keymap.set("n", "<M-p>", function() harpoon:list():prev() end, { desc = "Harpoon Prev" })
+  end,
+}
